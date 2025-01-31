4ecadb1cd4534e582e7b341f2f8840590b97d871
ori-milstein <mydoal@keemail.me>
2025-02-01 00:10:36 +0200

    Improve login button styling

--- src/cmps/HeaderUserControls.jsx
+++ src/cmps/HeaderUserControls.jsx
@@ -30,7 +30,7 @@ export function HeaderUserControls({ onToggleMenu, onAddStay, user }) {
                         </svg>
                         {user?.fullname ?
                             <div className='user-profile-letter'>
-                                <div>{user.fullname[0]}</div>
+                                <div>{user.fullname[0].toUpperCase()}</div>
                             </div>
                             :
                             <svg className="user-controls-profile" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"
