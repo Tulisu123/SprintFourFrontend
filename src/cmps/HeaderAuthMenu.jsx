67d93e3a646958605146d9ea60ca83514866cdba
ori-milstein <mydoal@keemail.me>
2025-08-06 15:33:42 +0300

    Hide logo when viewport is small

--- src/cmps/HeaderAuthMenu.jsx
+++ src/cmps/HeaderAuthMenu.jsx
@@ -1,18 +1,18 @@
 import { useSelector } from "react-redux"
 
-export function HeaderAuthMenu({ onToggleLoginSignupDialog,onUserLogout,onManageBooking }) {
-	const user = useSelector((storeState) => storeState.userModule.user)
-    if(user) return (
+export function HeaderAuthMenu({ onToggleLoginSignupDialog, onUserLogout, onManageBooking }) {
+    const user = useSelector((storeState) => storeState.userModule.user)
+    if (user) return (
         <div className="auth-menu">
-            <label className="auth-menu-item" onClick={onUserLogout}>Logout</label>
-            <label className="auth-menu-item" onClick={onManageBooking}>Manage your places</label>
+            <div className="auth-menu-item" onClick={onUserLogout}>Logout</div>
+            <div className="auth-menu-item" onClick={onManageBooking}>Manage your places</div>
 
         </div>
     )
     return (
         <div className="auth-menu">
-            <label className="auth-menu-item login-action" onClick={() => onToggleLoginSignupDialog('login')}>Log in</label>
-            <label className="auth-menu-item" onClick={() => onToggleLoginSignupDialog('signup')}>Sign up</label>
+            <div className="auth-menu-item login-action" onClick={() => onToggleLoginSignupDialog('login')}>Log in</div>
+            <div className="auth-menu-item" onClick={() => onToggleLoginSignupDialog('signup')}>Sign up</div>
         </div>
     )
 }
